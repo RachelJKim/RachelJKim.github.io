bf23712d2cac087198e8fccacc4d03eb1667519d
Rachel J.Kim <racheljk02@gmail.com>
2024-10-12 23:41:02 +0900

    updated publications page

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// function toggleMenu() {
-//     const menu = document.querySelector(".menu-links");
-//     const icon = document.querySelector(".hamburger-icon");
-//     menu.classList.toggle("open");
-//     icon.classList.toggle("open");
-// }
-
-// script.js or inline in <script> tags
 function toggleMenu() {
-    const menuLinks = document.querySelector('#hamburger-nav .menu-links');
-    menuLinks.classList.toggle('active');
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
 }
+
+// // script.js or inline in <script> tags
+// function toggleMenu() {
+//     const menuLinks = document.querySelector('#hamburger-nav .menu-links');
+//     menuLinks.classList.toggle('active');
+// }
