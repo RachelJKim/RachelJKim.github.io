586f14822cd51d89c2b3cf4020f39f2477dede5a
Rachel J.Kim <racheljk02@gmail.com>
2024-10-12 23:16:06 +0900

    updated file names

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+// function toggleMenu() {
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+// }
+
+// script.js or inline in <script> tags
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+    const menuLinks = document.querySelector('#hamburger-nav .menu-links');
+    menuLinks.classList.toggle('active');
 }
