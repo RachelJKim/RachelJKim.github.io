d23e7cbf0cbbb47a4658e1d0c33960e5c7c151a2
Rachel J.Kim <racheljk02@gmail.com>
2024-10-13 02:22:05 +0900

    hover behavior added

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-// // script.js or inline in <script> tags
-// function toggleMenu() {
-//     const menuLinks = document.querySelector('#hamburger-nav .menu-links');
-//     menuLinks.classList.toggle('active');
-// }
+/* 2. Hover Upon Profile Picture */
+const profilePic = document.querySelector('.profile-pic');
+const body = document.body;
+const originalSrc = profilePic.src;
+const hoverSrc = './assets/images/rachel/profile-pic-green-cropped.png'
+
+profilePic.addEventListener('mouseenter', () => {
+    body.classList.add('hover-mode');
+    profilePic.src = hoverSrc; // Change to the alternate image on hover
+});
+
+profilePic.addEventListener('mouseleave', () => {
+    body.classList.remove('hover-mode');
+    profilePic.src = originalSrc; // Revert to the original image when hover ends
+});
