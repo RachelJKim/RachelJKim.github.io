c694d67f8b4437342a0e52a35314ac9c9ce8be51
Rachel J.Kim <racheljk02@gmail.com>
2025-01-23 15:45:19 +0900

    mobile hamburger menu updated

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 /* 1. Toggle Hamburger Menu */
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
-    icon.classList.toggle("open");
 }
 
 /* 2. Hover Upon Profile Picture */
